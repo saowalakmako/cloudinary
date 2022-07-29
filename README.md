824a29dab3065bada953dc580ed3a2aa03d85a21
saowalakmako <saowalakmako@proton.me>
2022-07-29 10:18:13 +0000

    add input

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 https://github.com/netlify/explorers-up-and-running-with-serverless-functions/tree/06-end
 
+To run
+------
+- Need netlify-cli installed global
+- run with *netlify dev*
---
+++ functions/hash.js
@@ -0,0 +1,10 @@
+export const handler = async (event, context) => {
+    const eventBody = JSON.parse(event.body);
+    console.log(eventBody);
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello world OK!'
+        })
+    }
+}
