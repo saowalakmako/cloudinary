b6f72a8350507c6edf5a4b9cc7631e68123497ff
saowalakmako <saowalakmako@proton.me>
2022-07-29 15:26:23 +0000

    upload

--- functions/hash.js
+++ functions/hash.js
@@ -1,10 +1,17 @@
+import crypto from 'crypto';
+
 export const handler = async (event, context) => {
     const eventBody = JSON.parse(event.body);
-    console.log(eventBody);
+    const timestamp = Date.now() / 1000 | 0;
+    const input = `timestamp=${timestamp}${process.env.CLOUDINARY_API_SECRET}`;
+    const hash = crypto.createHash('sha256').update(input).digest('hex');
+
+    console.log(eventBody, hash);
+
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello world OK!'
+            hash, timestamp, api_key: process.env.CLOUDINARY_API_KEY
         })
     }
 }
